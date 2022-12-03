252be3609f7df40a9d844d3ee3f258348d516686
Dhanawat Chernsakolwong <1570040+iqcang@users.noreply.github.com>
2022-12-03 15:50:07 +0700

    --url /docs

--- docs/runtime.js
+++ docs/runtime.js
@@ -232,7 +232,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
-/******/ 		__webpack_require__.p = "";
+/******/ 		__webpack_require__.p = "/docs";
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/jsonp chunk loading */
